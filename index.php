30280fdab9c40a9770a76ea6b1d2b5d1e0a23bcd
AndreanBagusSaputra <saputraandreanbagus@gmail.com>
2023-03-24 11:27:37 +0700

    Tugas 11 PHP

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Pajak</title>
+</head>
+<body>
+    <form action="" method="post">
+        <form action="" method="post"><
+        <p> Form Pajak</p>
+        <label for="">Nama</label>
+            <input type="text" name="nama" placeholder="Masukan Nama" id=""><br>
+        <label for="">Jabatan</label>
+        <select name="jabatan" id="">
+            <option>- Pilih Jabatan -</option>
+            <option value="manager">Manager</option>
+            <option value="asisten">Asisten</option>
+            <option value="kabag">Kepala Bagian</option>
+            <option value="staff">Staff</option>
+        </select><br>
+        <label for="">Status</label>
+        <select name="status" id="">
+            <option>- Status -</option>
+            <option value="menikah">Menikah</option>
+            <option value="blm">Belum Menikah</option>
+        </select><br>
+        <label for="">Jumlah Anak</label>
+        <select name="jumlah" id="">
+            <option>- Jumlah Anak -</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value=">5">>5</option>
+        </select><br>
+        
+        <button type="submit" name="proses">Simpen</button>
+    </form>
+    </form>
+</body>
+</html>
