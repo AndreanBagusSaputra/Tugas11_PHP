a63d2fa1d7e8ff463b19449c4f53651a87e56881
AndreanBagusSaputra <saputraandreanbagus@gmail.com>
2023-03-27 01:42:48 +0700

    Update

--- index.php
+++ index.php
@@ -8,24 +8,24 @@
 </head>
 <body>
     <form action="" method="post">
-        <form action="" method="post"><
-        <p> Form Pajak</p>
+        <form action="" method="post">
+        <h1>Form Pajak</h1>
         <label for="">Nama</label>
-            <input type="text" name="nama" placeholder="Masukan Nama" id=""><br>
+            <input type="text" name="nama" placeholder="Masukan Nama" id=""><br><br>
         <label for="">Jabatan</label>
         <select name="jabatan" id="">
             <option>- Pilih Jabatan -</option>
-            <option value="manager">Manager</option>
-            <option value="asisten">Asisten</option>
-            <option value="kabag">Kepala Bagian</option>
-            <option value="staff">Staff</option>
-        </select><br>
+            <option value="Manager">Manager</option>
+            <option value="Asisten">Asisten</option>
+            <option value="Kepala Bagian">Kepala Bagian</option>
+            <option value="Staff">Staff</option>
+        </select><br><br>
         <label for="">Status</label>
         <select name="status" id="">
             <option>- Status -</option>
-            <option value="menikah">Menikah</option>
-            <option value="blm">Belum Menikah</option>
-        </select><br>
+            <option value="Menikah">Menikah</option>
+            <option value="Belum Menikah">Belum Menikah</option>
+        </select><br><br>
         <label for="">Jumlah Anak</label>
         <select name="jumlah" id="">
             <option>- Jumlah Anak -</option>
@@ -34,11 +34,57 @@
             <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
-            <option value=">5">>5</option>
-        </select><br>
+        </select><br><br>
+        <label for="">Agama</label>
+        <select name="agama" id="">
+            <option value="Muslim">Muslim</option>
+            <option value="Non muslim">Non muslim</option>
+        </select><br><br>
         
-        <button type="submit" name="proses">Simpen</button>
+        <button type="submit" name="proses">Simpan</button>
     </form>
-    </form>
+
+    <?php
+    $nama = $_POST ['nama'];
+    $jabatan = $_POST ['jabatan'];
+    $status = $_POST ['status'];
+    $jml_anak = $_POST ['jumlah'];
+    $agama = $_POST ['agama'];
+    $tombol = $_POST ['proses'];
+
+    switch ($jabatan){
+        case "Manager" : $gaji_pokok = 20000000; break;
+        case "Asisten" : $gaji_pokok = 15000000; break;
+        case "Kepala Bagian " : $gaji_pokok = 10000000; break;
+        case "Staff" : $gaji_pokok = 4000000; break;
+        default : $gaji_pokok = 0;
+    }
+
+    if ($status == "Menikah" && $jml_anak <= 2) $tng_keluarga = 0.05 * $gaji_pokok;
+    else if ($status == "Menikah" && $jml_anak <= 5) $tng_keluarga =  0.1 * $gaji_pokok;
+    else $tng_keluarga = "-";
+
+    $bruto = $gaji_pokok + $tng_keluarga;
+
+    $zakat = ($agama == "Muslim" && $bruto >= 6000000) ? (0.025 * $bruto ) : 0;
+
+    $take_home_pay = $bruto - $zakat;
+
+    if (isset($tombol)){
+        ?>
+        Nama : <?= $nama ?>
+        <br> Jabatan : <?= $jabatan ?>
+        <br> Gaji Pokok : <?=$gaji_pokok?>
+        <br> Agama : <?=$agama?>
+        <br> Status : <?= $status ?>
+        <br> Jumlah Anak : <?= $jml_anak ?>
+        <br> Tunjangan Keluarga : <?= $tng_keluarga?>
+        <br> Gaji Kotor : <?=$bruto?>
+        <br> Zakat : <?=$zakat?>
+        <br> Gaji Bersih : <?=$take_home_pay?>
+    <?php
+    }
+
+?>
 </body>
 </html>
